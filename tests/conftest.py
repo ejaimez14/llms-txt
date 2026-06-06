964404e52e34ac5aff4a7b3e6ce70aa9c11449d1
ejaimez14 <erickjaimez350@gmail.com>
2026-06-06 14:08:54 -0700

    Add AWS region and creds to conftest for module-level boto3 init

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,8 @@
 import os
 
+os.environ.setdefault("AWS_DEFAULT_REGION", "us-east-1")
+os.environ.setdefault("AWS_ACCESS_KEY_ID", "testing")
+os.environ.setdefault("AWS_SECRET_ACCESS_KEY", "testing")
 os.environ.setdefault("BUCKET", "test-bucket")
 os.environ.setdefault("TABLE", "test-jobs")
 os.environ.setdefault("SITES_TABLE", "test-sites")
